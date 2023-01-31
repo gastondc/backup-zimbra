c165d9aa6fb2bbe664dd96e462cb1519fdf6f93f
Gaston Carballo <gastondc@gmail.com>
2023-01-31 12:05:14 -0300

    fix mkdir and retention variable

--- zimbra-backup.sh
+++ zimbra-backup.sh
@@ -7,6 +7,7 @@ DATE=`date +%Y-%m-%d`
 REMOTE_SERVER=user@remote-server.com
 REMOTE_DIR=/backup/zimbra
 LOG_FILE=/var/log/zimbra-backup.log
+RETENTION=2
 
 log_check_message() {
     echo "$1 on $(date)" >> $LOG_FILE
@@ -17,7 +18,7 @@ log_check_message() {
 }
 
 # Create daily backup directory
-mkdir -p ${BACKUP_HOME}/daily/${DATE}
+su - zimbra -c "mkdir -p ${BACKUP_HOME}/daily/${DATE}"
 log_check_message "Creating backup directory"
 
 # Listar usuarios 
@@ -27,6 +28,7 @@ users=$(su - zimbra -c "zmprov -l gaa")
 # Backup Zimbra 
 for i in $users
 do 
+  
   su - zimbra -c "zmmailbox -z -m $i getRestURL \"//?fmt=tgz\" > ${BACKUP_HOME}/daily/${DATE}/$i.tgz"
   log_check_message "Zimbra Backup $i"
 done
@@ -39,8 +41,8 @@ done
 scp -r ${BACKUP_HOME}/daily/${DATE} ${REMOTE_SERVER}:${REMOTE_DIR}
 log_check_message "SCP Backup"
 
-# Delete backups older than 30 days
-find ${BACKUP_HOME}/daily/* -mtime +30 -exec rm -rf {} \;
+# Delete backups older than $RETENTION VARIABLE days
+find ${BACKUP_HOME}/daily/* -mtime +$RETENTION -exec rm -rf {} \;
 log_check_message "Deleting old backups"
 
 log_check_message "Backup completed successfully"
